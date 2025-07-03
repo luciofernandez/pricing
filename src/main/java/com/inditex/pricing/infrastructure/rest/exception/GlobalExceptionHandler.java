75985b71097007c75318b4d9b4b155b21b337ab3
Lucio <luciofernandez23@gmail.com>
2025-07-03 10:45:34 +0200

    last modified

--- src/main/java/com/inditex/pricing/infrastructure/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/inditex/pricing/infrastructure/rest/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.inditex.pricing.infrastructure.rest.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.inditex.pricing.domain.exception.BrandPriceNotFoundException;
-import com.inditex.pricing.infrastructure.rest.controller.BrandPriceController;
 import com.inditex.pricing.infrastructure.rest.mapper.ErrorResponseMapper;
 import com.inditex.pricing.model.ErrorResponse;
 
@@ -18,8 +15,6 @@ import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 	@ExceptionHandler(BrandPriceNotFoundException.class)
 	public ResponseEntity<ErrorResponse> handleValidationNotFoundStatus(BrandPriceNotFoundException ex) {
